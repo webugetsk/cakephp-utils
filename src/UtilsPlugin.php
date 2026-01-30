19c03173fd9965091425526209444745e70f55c0
Alim Imanbakiev <alim.imanbakiev@webuget.sk>
2026-01-30 23:36:30 +0100

    Add UtilsPlugin skeleton

---
+++ src/UtilsPlugin.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Utils;
+
+use Cake\Console\CommandCollection;
+use Cake\Core\BasePlugin;
+use Cake\Core\ContainerInterface;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\RouteBuilder;
+
+/**
+ * Plugin for Tools
+ */
+class UtilsPlugin extends BasePlugin
+{
+
+}
